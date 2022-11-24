3bad9d0045e1c4ff87a26634575d53ecf66bda17
richie-wu <richie.mail.wu@gmail.com>
2022-11-24 13:42:19 -0500

    added stuff

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>my self intro</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          tktktk introduction
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
